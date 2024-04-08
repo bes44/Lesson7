ae4a982f6af165d02fdeece87b496aa275c648fe
Eugene Berezin <esberezin@gmail.com>
2024-04-08 23:36:59 +0500

    Задания к 7 семинару

---
+++ Task01/Program.cs
@@ -0,0 +1,20 @@
+﻿ Console.Write("Введите начальное число (M): ");
+    int M = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите конечное число (N): ");
+    int N = Convert.ToInt32(Console.ReadLine());
+    Console.Write($"все натуральные числа в промежутке от {M} до {N}: ");
+    SimpleNumbers(M, N);
+
+
+void SimpleNumbers(int M, int N)
+{
+    if(M > N) return;
+    if (M>0) {
+    Console.Write($"{M}  ");
+    }
+    SimpleNumbers(M+1, N);
+}
+
+
+   
+
